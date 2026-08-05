3a70764da6465418ea257d76b03ddb466cb84aec
oOBobbyOo <944831575@qq.com>
2026-08-05 19:35:22 +0800

    perf: entity JsonInclude NON_NULL

--- springboot-mybatis-starter/src/main/java/com/company/mybatis/entity/Clazz.java
+++ springboot-mybatis-starter/src/main/java/com/company/mybatis/entity/Clazz.java
@@ -1,18 +1,25 @@
 package com.company.mybatis.entity;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 /**
  * @author Henry
  */
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Clazz {
 
   private Integer id;
 
   private String name;
 
-  /** 班级下的学生 一对多关系：一个班级有多个学生 */
+  /**
+   * 班级下的学生
+   *
+   * <p>一对多关系：一个班级有多个学生
+   */
   private List<Student> students;
 }
--- springboot-mybatis-starter/src/main/java/com/company/mybatis/entity/Student.java
+++ springboot-mybatis-starter/src/main/java/com/company/mybatis/entity/Student.java
@@ -1,11 +1,13 @@
 package com.company.mybatis.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 /**
  * @author Henry
  */
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Student {
 
   private Integer id;
@@ -16,6 +18,9 @@ public class Student {
 
   private Integer classId;
 
-  /** 学生所属班级 多对一关系：一个学生属于一个班级 */
+  /**
+   * 学生所属班级
+   * <p>多对一关系：一个学生属于一个班级
+   */
   private Clazz clazz;
 }
