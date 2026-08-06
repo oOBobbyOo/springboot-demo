5c03e866d08bc5690d23c7fc52dcd51364c22aa0
oOBobbyOo <944831575@qq.com>
2026-08-06 18:35:29 +0800

    feat: springboot-mybatis-plus

---
+++ springboot-mybatis-plus/src/main/java/com/company/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.company.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Henry
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
