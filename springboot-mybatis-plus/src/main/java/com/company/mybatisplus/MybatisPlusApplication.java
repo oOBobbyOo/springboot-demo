06c65ccc7b57135f929f1e6aab83e1cc2e8ce5d1
oOBobbyOo <944831575@qq.com>
2026-08-11 10:49:55 +0800

    perf: change MybatisPlusConfig

--- springboot-mybatis-plus/src/main/java/com/company/mybatisplus/MybatisPlusApplication.java
+++ springboot-mybatis-plus/src/main/java/com/company/mybatisplus/MybatisPlusApplication.java
@@ -1,6 +1,5 @@
 package com.company.mybatisplus;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Henry
  */
 @SpringBootApplication
-@MapperScan("com.company.mybatisplus.**.mapper")
 public class MybatisPlusApplication {
 
      static void main(String[] args) {
--- springboot-mybatis-plus/src/main/java/com/company/mybatisplus/config/MybatisPlusConfig.java
+++ springboot-mybatis-plus/src/main/java/com/company/mybatisplus/config/MybatisPlusConfig.java
@@ -2,6 +2,8 @@ package com.company.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -26,7 +28,23 @@ public class MybatisPlusConfig {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
 
     // 分页插件
-    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+    // 设置最大单页限制数量，默认 500 条，-1 不受限制
+    paginationInnerInterceptor.setMaxLimit(500L);
+    // 溢出总页数后是否进行处理，默认 false，当当前页大于总页数时，自动回到第一页
+    paginationInnerInterceptor.setOverflow(false);
+    // 单页分页结果统计优化
+    paginationInnerInterceptor.setOptimizeJoin(true);
+    interceptor.addInnerInterceptor(paginationInnerInterceptor);
+
+    // 乐观锁插件
+    interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+
+    // 防止全表更新与删除插件
+    interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+
+    // 性能分析插件（生产环境建议关闭）
+    // interceptor.addInnerInterceptor(new IllegalSQLInnerInterceptor());
 
     return interceptor;
   }
